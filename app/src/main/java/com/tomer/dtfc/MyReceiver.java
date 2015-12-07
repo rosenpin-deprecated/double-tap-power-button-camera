a37f5513a59a8c884f9f888c2348bd7c24faaa42
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2015-12-07 23:32:15 +0200

    Now works if there is a lockscreen

--- app/src/main/java/com/tomer/dtfc/MyReceiver.java
+++ app/src/main/java/com/tomer/dtfc/MyReceiver.java
@@ -1,21 +1,24 @@
 package com.tomer.dtfc;
 
+import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.os.PowerManager;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * Created by tomer on 11/7/15.
  */
 public class MyReceiver extends BroadcastReceiver {
     static int countPowerOff = 0;
+    PowerManager pm;
+    KeyguardManager keyguardManager;
+    KeyguardManager.KeyguardLock keyguardLock;
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
         public void run() {
@@ -26,13 +29,25 @@ public class MyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context,"STARTED SERVICE",Toast.LENGTH_LONG).show();
-        Log.v("onReceive", "Power button is pressed."+countPowerOff);
+        if (pm == null) {
+            pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "Camera Open");
+            wl.acquire();
+            keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+            keyguardLock = keyguardManager.newKeyguardLock("TAG");
+        }
+        Toast.makeText(context, "STARTED SERVICE", Toast.LENGTH_LONG).show();
+        Log.v("onReceive", "Power button is pressed." + countPowerOff);
         countPowerOff++;
         handler.postDelayed(runnable, context.getResources().getInteger(R.integer.timeout));
         if (countPowerOff == 2) {
+            keyguardLock.reenableKeyguard();
+            keyguardLock.disableKeyguard();
+
             System.out.println("OPENING CAMERA");
-            context.startActivity(new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA));
+            Intent cameraIntent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+
+            context.startActivity(cameraIntent);
             countPowerOff = 0;
             handler.removeCallbacks(runnable);
         }
